9831da3328d2af85db2d34c3f21f2e08782b67a1
Peter Winskill <p.winskill@imperial.ac.uk>
2021-03-09 08:43:31 +0000

    subplotting output

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -5,11 +5,5 @@ test_that("plotting works", {
 
   # All variables
   p1 <- plot_sim(test_output_long)
-  expect_equal(is(p1), "gg")
-  expect_equal(length(unique(ggplot2::ggplot_build(p1)$data[[1]]$PANEL)), 2)
-
-  # Single variable selected
-  p2 <- plot_sim(test_output_long, vars = "var1")
-  expect_equal(is(p2), "gg")
-  expect_equal(length(unique(ggplot2::ggplot_build(p2)$data[[1]]$PANEL)), 1)
+  expect_equal(is(p1), "patchwork")
 })

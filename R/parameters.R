28d741a3ff660f8b6d13316caadfad7aeb3bacb2
Peter Winskill <p.winskill@imperial.ac.uk>
2021-03-11 13:09:31 +0000

    Fix bugged life course

--- R/parameters.R
+++ R/parameters.R
@@ -22,7 +22,7 @@ get_parameters <- function(){
     het_sd = 1.6,
     # Interventions
     llin_coverage = 0.5,
-    rotavirus_vx_coverage = 1,
+    rotavirus_vx_coverage = 0,
     pneumococcal_vx_coverage = 0.1,
     hib_vx_coverage = 0.1
   )
@@ -31,21 +31,21 @@ get_parameters <- function(){
     groups = 4,
     type = c("bacteria", "virus", "parasite", "rotavirus"),
     index = c(1, 2, 3, 4),
-    sigma = c(0.01, 0.02, 0.03, 0.04),
+    sigma = c(0.02, 0.02, 0.02, 0.02),
     # Average duration of clinical episode
     clin_dur = c(14, 14, 14, 14),
     # Maternal immunity
     ## Half life
-    mi_hl =  c(100, 200, 100, 100),
+    mi_hl =  c(100, 100, 100, 100),
     # Infection immunity
     ## shape
-    ii_shape = c(7, 5, 6, 4),
+    ii_shape = c(5, 5, 5, 5),
     ## rate
-    ii_rate = c(10, 0.5, 3, 6),
+    ii_rate = c(1, 1, 1, 1),
     # Vaccination
     vx_start = c(0, 0, 0, 0),
-    vx_initial_efficacy = c(0, 0, 0, 1),
-    vx_hl = c(0, 0, 0, 365 * 100),
+    vx_initial_efficacy = c(0, 0, 0, 0),
+    vx_hl = c(0, 0, 0, 0),
     # TODO this should be a function of coverage:
     vx_ci = c(0, 0, 0, 0)
   )

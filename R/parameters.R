68599bbeddf11f100a4aacfa709e866e9a372091
Peter Winskill <p.winskill@imperial.ac.uk>
2021-04-15 13:30:50 +0100

    ÂPneumonia parameter placeholders

--- R/parameters.R
+++ R/parameters.R
@@ -64,6 +64,41 @@ get_parameters <- function(user_overwrite = NULL){
     symptom_time_refer = 5
   )
 
+  parameters$pneumonia <- list(
+    groups = 5,
+    disease = c("bacteria", "virus", "funus", "pneumococcus", "hib"),
+    index = c(1, 2, 3, 4, 5),
+    sigma = c(0.005, 0.005, 0.005, 0.005, 0.005),
+    # Average duration of clinical episode
+    clin_dur = c(14, 14, 14, 14, 14),
+    asymp_dur = c(0, 0, 0, 0, 0),
+    daily_prob_severe = c(0.1, 0.2, 0.3, 0, 0.1),
+    daily_prob_death = c(0.001, 0.002, 0.003, 0, 0.0001),
+    prob_fever = c(0.5, 0.5, 0.5, 0.5, 0.5),
+    # Maternal immunity
+    ## Half life
+    mi_hl = c(100, 100, 100, 100, 100),
+    # Infection immunity
+    ## shape
+    ii_shape = c(5, 5, 5, 5, 5),
+    ## rate
+    ii_rate = c(1, 1, 1, 1, 1),
+    # Clinical immunity
+    ## shape
+    ci_shape = c(0, 0, 0, 0, 0),
+    ## rate
+    ci_rate = c(0, 0, 0, 0, 0),
+    # Vaccination
+    vx_start = c(0, 0, 0, 0, 0),
+    vx_initial_efficacy = c(0, 0, 0, 0, 0),
+    vx_hl = c(0, 0, 0, 0, 0),
+    # TODO this should be a function of coverage:
+    vx_ci = c(0, 0, 0, 0, 0),
+    # Treatment prophylaxis
+    prophylaxis_hl = c(30, 0, 0, 14, 0),
+    symptom_time_refer = 5
+  )
+
   parameters$malaria <- list(
     groups = 1,
     disease = "pf",
